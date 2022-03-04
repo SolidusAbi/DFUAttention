d51e04e76fcb2e1351dc2a6a916dc5bb90775152
Abián Hernández <abianhernandez@outlook.es>
2022-03-04 10:58:17 +0000

    Attention mechanishm applied to DFU

---
+++ config.py
@@ -0,0 +1,5 @@
+r'''
+    Contiene variables de entorno para el proyecto      
+'''
+
+DATASET_DIR = '/home/abian/Workspace/Dataset/DiabeticFootDataset/'
---
+++ dataset/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from .DFU import DiabeticFootSubject, DiabeticFootData, DiabeticFootDataset, DiabeticFootTorchDataset
